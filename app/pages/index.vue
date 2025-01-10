09d7af5eb7ac8fa9492bb211cdafd1583e191a4a
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-10 21:52:13 +0300

    Fixed auth middleware & optimized seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
-  
+useSeoMeta({
+  title: 'CollabFile',
+  description: 'Share file from every place',
+  ogTitle: 'CollabFile',
+  ogDescription: 'Share file from every place',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }
+  ]
+})
 </script>
 
 <template>
