e0448012e06f61b72e492122b3a801dbaaf8c51f
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-10 20:43:29 +0300

    Created login route

---
+++ server/api/auth/login.post.ts
@@ -0,0 +1,60 @@
+import { verify } from "@node-rs/argon2";
+import { eq } from "drizzle-orm";
+import * as v from "valibot";
+import { cookieName } from "~~/server/constants";
+import { db } from "~~/server/database";
+import { userTable } from "~~/server/database/schema";
+import { createSession, generateToken } from "~~/server/session";
+
+const bodySchema = v.object({
+  username: v.pipe(v.string(), v.nonEmpty("Username is required")),
+  password: v.pipe(v.string(), v.nonEmpty("Password is required")),
+});
+
+export default defineEventHandler(async (event) => {
+  const body = await readValidatedBody(event, (body) =>
+    v.safeParse(bodySchema, body)
+  );
+
+  if (!body.success) {
+    throw createError({
+      statusCode: 400,
+      data: body.issues,
+      message: "Bad Request",
+    });
+  }
+
+  const [user] = await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.username, body.output.username));
+
+  if (!user) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Not Found",
+      message: "User not found",
+    });
+  }
+
+  const isValidPassword = await verify(body.output.password, user.password);
+
+  if (!isValidPassword) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+      message: "Invalid credentials",
+    });
+  }
+
+  const token = generateToken();
+  createSession(user.id, token);
+
+  setCookie(event, cookieName, token, {
+    secure: true,
+    sameSite: "lax",
+    httpOnly: true,
+  });
+
+  return sendNoContent(event);
+});
