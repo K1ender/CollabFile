1f4582829b0429238bf4c5133b38c3fa13a0657e
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-10 18:24:25 +0300

    Create user schema and session table

---
+++ server/database/schema.ts
@@ -0,0 +1,32 @@
+import { text } from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/pg-core";
+import { integer } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("users", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  username: text("username").notNull().unique(),
+  password: text("password").notNull(),
+});
+
+export const filesTable = pgTable("files", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userID: integer("userID").references(() => userTable.id, {
+    onDelete: "cascade",
+    onUpdate: "cascade",
+  }),
+  // TODO: check what returns s3 for id
+});
+
+export const sessionTable = pgTable("sessions", {
+  id: text("id").primaryKey(),
+  userID: integer("userID")
+    .notNull()
+    .references(() => userTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+  expiresAt: timestamp("expiresAt", {
+    withTimezone: true,
+  }).notNull(),
+});
