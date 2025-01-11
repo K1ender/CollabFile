d833fdc42f6c45dbf03f2e0826d2f16ba0eaef8b
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-11 23:14:32 +0300

    🧹Chore

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -15,12 +15,14 @@ async function logout() {
     <div class="pt-21 flex flex-col items-center min-h-full">
         <h1 class="text-center text-3xl fw-semibold">Welcome back! {{ user?.username }}</h1>
 
+        <Button class="my-2">Create temporary link</Button>
+
         <div>
             <h2>Your files: </h2>
             <ul></ul>
         </div>
 
-        <Button>Create temporary link</Button>
+
 
         <Button class="mt-4" @click="logout">Logout</Button>
     </div>
