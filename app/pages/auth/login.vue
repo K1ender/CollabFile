06b79741c189fd8917a7611b1e8735fa6a692d4e
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-11 22:08:02 +0300

    Created loading state

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -3,15 +3,19 @@
 const username = ref("");
 const password = ref("");
 
+const isLoading = ref(false);
+
 const { login: addAuth } = useUser();
 
 async function login() {
     try {
+        isLoading.value = true;
         await addAuth(username.value, password.value);
         navigateTo("/")
     } catch (error) {
         console.log(error);
     }
+    isLoading.value = false;
 }
 
 </script>
@@ -21,7 +25,7 @@ async function login() {
         <form @submit.prevent="login" class="flex flex-col gap-4 w-1/3 w-xs">
             <Input v-model="username" class="text-center" placeholder="Username" type="text" />
             <Input v-model="password" class="text-center" placeholder="Password" type="password" />
-            <Button>Login</Button>
+            <Button :disabled="isLoading">{{ isLoading ? "Loading..." : "Login" }}</Button>
         </form>
     </div>
 </template>
--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+const username = ref("");
+const password = ref("");
+
+const { register: createUser } = useUser();
+
+const isLoading = ref(false);
+
+async function register() {
+    try {
+        isLoading.value = true;
+        await createUser(username.value, password.value);
+        navigateTo("/")
+    } catch (error) {
+        console.log(error);
+    }
+    isLoading.value = false;
+}
+
+</script>
+
 <template>
-    Register page
+    <div class="md:container mx-auto h-stretch flex items-center justify-center flex h-full">
+        <form @submit.prevent="register" class="flex flex-col gap-4 w-1/3 w-xs">
+            <Input v-model="username" class="text-center" placeholder="Username" type="text" />
+            <Input v-model="password" class="text-center" placeholder="Password" type="password" />
+            <Button :disabled="isLoading">{{ isLoading ? "Loading..." : "Register" }}</Button>
+        </form>
+    </div>
 </template>
