d4517a2602bf2c6462fdb3c9d8abaa23a66fb5f4
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-13 22:25:55 +0300

    🧹 Chore: Some UI improvements

--- app/pages/uploadFile/[id].vue
+++ app/pages/uploadFile/[id].vue
@@ -13,7 +13,6 @@ let intervalId: number;
 
 onMounted(() => {
     intervalId = window.setInterval(() => {
-
         refresh();
     }, 1000 * 60);
 })
@@ -60,14 +59,14 @@ async function uploadFile() {
         console.log(error);
     }
 
-    // navigateTo("/");
+    navigateTo("/");
     isLoading.value = false;
 }
 
 </script>
 
 <template>
-    <div class="text-center pt-22 flex flex-col items-center">
+    <div class="text-center flex flex-col items-center w-full h-full justify-center">
         <div v-if="error">
             <h1>404</h1>
             <p>File not found</p>
@@ -79,12 +78,10 @@ async function uploadFile() {
             <p class="mt-4">Max file size: 100 MB </p>
             <input ref="fileUpload" type="file" />
 
-            <Button :disabled="isLoading" @click="uploadFile" class="my-2">
+            <Button :disabled="isLoading" @click="uploadFile" class="mt-6">
                 {{ isLoading ? "Uploading..." : "Upload" }}
             </Button>
-
-
-            <NuxtLink class="text-blue-500 mt-4" to="/">Back</NuxtLink>
+            <Button variant="link" class="text-blue-500 mt-2" to="/profile">Back</Button>
         </div>
     </div>
 </template>
