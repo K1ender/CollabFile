541b3df1e024cea47d2142b27522f92140d421f5
K1ender <146767945+K1ender@users.noreply.github.com>
2025-01-26 13:56:53 +0300

    feat: some security fixes

---
+++ server/middleware/headersMiddleware.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler((event) => {
+  event.node.res.setHeader("X-Content-Type-Options", "nosniff");
+  event.node.res.setHeader("X-Frame-Options", "DENY");
+  event.node.res.setHeader(
+    "Referrer-Policy",
+    "strict-origin-when-cross-origin",
+  );
+  event.node.res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://va.vercel-scripts.com 'unsafe-inline';",
+  );
+});
